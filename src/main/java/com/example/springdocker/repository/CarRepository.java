05dffd2f0ad778414e64b57ebf8c9cb156afecf3
Salah Abdinoor <Salah.abdinoor@hotmail.com>
2021-05-18 04:09:25 +0200

    repository: CarRepository added

---
+++ src/main/java/com/example/springdocker/repository/CarRepository.java
@@ -0,0 +1,20 @@
+package com.example.springdocker.repository;
+import com.example.springdocker.model.Car;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+/**
+ * Created by Salah Abdinoor
+ * 5/18/2021
+ * 4:06 AM
+ * spring-docker-demo
+ * Copyright: MIT
+ */
+@Repository
+public interface CarRepository extends MongoRepository<Car, String> {
+
+    List<Car> FindCarByEuropean(boolean isEuropean);
+    
+}
