b5a7adeab4d8f96707ca14e5c9e4e6c3be0ee509
Salah Abdinoor <70173421+SalahAbdinoor@users.noreply.github.com>
2021-05-19 12:56:01 +0200

    Added MyMathCalc.java (#1)
    Squash & Merge

---
+++ src/main/java/com/example/springdocker/MyMathCalc.java
@@ -0,0 +1,19 @@
+package com.example.springdocker;
+
+/**
+ * Created by Salah Abdinoor
+ * 5/19/2021
+ * 10:49 AM
+ * CarController.java
+ * Copyright: MIT
+ */
+public class MyMathCalc {
+
+    public int add(int a, int b){return a + b;}
+
+    public int multiply(int a, int b){return a * b;}
+    
+    public int divide(int a, int b){return a / b;}
+
+
+}
